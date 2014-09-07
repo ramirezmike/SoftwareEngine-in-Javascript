b79b6bd31f8b1dfc817ebc63641babeb8069f949
Michael Ramirez <ramirezmike2@gmail.com>
2014-09-07 00:17:23 -0400

    Filled triangles; added blender file

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var canvas;
 var device;
-var mesh;
-var mesh2;
 var meshes = [];
 var camera;
 
@@ -9,74 +7,27 @@ document.addEventListener("DOMContentLoaded", init, false);
 
 function init() {
     canvas = document.getElementById("frontBuffer");
-    mesh = new SoftEngine.Mesh("Cube", 8, 12);
-    mesh2 = new SoftEngine.Mesh("Cube", 8, 12);
-    meshes.push(mesh);
-    meshes.push(mesh2);
     camera = new SoftEngine.Camera();
     device = new SoftEngine.Device(canvas);
-
-    mesh.Vertices[0] = new BABYLON.Vector3(-1, 1, 1);
-    mesh.Vertices[1] = new BABYLON.Vector3(1, 1, 1);
-    mesh.Vertices[2] = new BABYLON.Vector3(-1, -1, 1);
-    mesh.Vertices[3] = new BABYLON.Vector3(1, -1, 1);
-    mesh.Vertices[4] = new BABYLON.Vector3(-1, 1, -1);
-    mesh.Vertices[5] = new BABYLON.Vector3(1, 1, -1);
-    mesh.Vertices[6] = new BABYLON.Vector3(1, -1, -1);
-    mesh.Vertices[7] = new BABYLON.Vector3(-1, -1, -1);
-
-    mesh.Faces[0] = { A:0, B:1, C:2 };
-    mesh.Faces[1] = { A:1, B:2, C:3 };
-    mesh.Faces[2] = { A:1, B:3, C:6 };
-    mesh.Faces[3] = { A:1, B:5, C:6 };
-    mesh.Faces[4] = { A:0, B:1, C:4 };
-    mesh.Faces[5] = { A:1, B:4, C:5 };
-    mesh.Faces[6] = { A:2, B:3, C:7 };
-    mesh.Faces[7] = { A:3, B:6, C:7 };
-    mesh.Faces[8] = { A:0, B:2, C:7 };
-    mesh.Faces[9] = { A:0, B:4, C:7 };
-    mesh.Faces[10] = { A:4, B:5, C:6 };
-    mesh.Faces[11] = { A:4, B:6, C:7 };
-
-    mesh2.Faces[0] = { A:0, B:1, C:2 };
-    mesh2.Faces[1] = { A:1, B:2, C:3 };
-    mesh2.Faces[2] = { A:1, B:3, C:6 };
-    mesh2.Faces[3] = { A:1, B:5, C:6 };
-    mesh2.Faces[4] = { A:0, B:1, C:4 };
-    mesh2.Faces[5] = { A:1, B:4, C:5 };
-    mesh2.Faces[6] = { A:2, B:3, C:7 };
-    mesh2.Faces[7] = { A:3, B:6, C:7 };
-    mesh2.Faces[8] = { A:0, B:2, C:7 };
-    mesh2.Faces[9] = { A:0, B:4, C:7 };
-    mesh2.Faces[10] = { A:4, B:5, C:6 };
-    mesh2.Faces[11] = { A:4, B:6, C:7 };
-
-    mesh2.Vertices[0] = new BABYLON.Vector3(-1, 1, 1);
-    mesh2.Vertices[1] = new BABYLON.Vector3(1, 1, 1);
-    mesh2.Vertices[2] = new BABYLON.Vector3(-1, -1, 1);
-    mesh2.Vertices[3] = new BABYLON.Vector3(1, -1, 1);
-    mesh2.Vertices[4] = new BABYLON.Vector3(-1, 1, -1);
-    mesh2.Vertices[5] = new BABYLON.Vector3(1, 1, -1);
-    mesh2.Vertices[6] = new BABYLON.Vector3(1, -1, -1);
-    mesh2.Vertices[7] = new BABYLON.Vector3(-1, -1, -1);
-
-    mesh2.Scaling.x = 2;
-    mesh2.Scaling.y = 2;
-    mesh2.Scaling.z = 2;
-
     camera.Position = new BABYLON.Vector3(-20, 0, 10);
     camera.Target = new BABYLON.Vector3(0, 0, 0);
+    device.LoadJSONFileAsync("mesh.js", loadJSONCompleted);
+}
 
+function loadJSONCompleted(meshesLoaded) {
+    meshes = meshesLoaded;
     requestAnimationFrame(drawingLoop);
 }
 
 function drawingLoop() {
     device.clear();
 
-    mesh.Rotation.x += 0.01;
-    mesh.Rotation.y += 0.01;
-    mesh2.Rotation.x -= 0.01;
-    mesh2.Rotation.y -= 0.01;
+    for (var i = 0; i < meshes.length; i++) {
+        meshes[i].Rotation.y += 0.01;
+        meshes[i].Scaling.x = 3;
+        meshes[i].Scaling.y = 3;
+        meshes[i].Scaling.z = 3;
+    }
 
     device.render(camera, meshes);
     device.present();
--- softEngine.js
+++ softEngine.js
@@ -31,40 +31,206 @@ var SoftEngine;
        	    this.workingWidth = canvas.width;
        	    this.workingHeight = canvas.height;
        	    this.workingContext = this.workingCanvas.getContext("2d");
-       	}
+            this.depthbuffer = new Array(this.workingWidth * this.workingHeight);
+       	};
+
+       	Device.prototype.LoadJSONFileAsync = function(fileName, callback) {
+               var jsonObject = {};
+               var xmlhttp = new XMLHttpRequest();
+               xmlhttp.open("GET", fileName, true);
+               var device = this;
+               xmlhttp.onreadystatechange = function() {
+                   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                       jsonObject = JSON.parse(xmlhttp.responseText);
+                       callback(device.CreateMeshesFromJSON(jsonObject));
+                   }
+               };
+               xmlhttp.send(null);
+        };
+
+        Device.prototype.CreateMeshesFromJSON = function (jsonObject) {
+            var meshes = [];
+            for (var meshIndex = 0; meshIndex < jsonObject.meshes.length; meshIndex++) {
+                var verticesArray = jsonObject.meshes[meshIndex].vertices;
+                //Faces
+                var indicesArray = jsonObject.meshes[meshIndex].indices;
+
+                var uvCount = jsonObject.meshes[meshIndex].uvCount;
+                var verticesStep = 1;
+
+                switch (uvCount) {
+                    case 0:
+                        verticesStep = 6;
+                        break;
+                    case 1:
+                        verticesStep = 8;
+                        break;
+                    case 2:
+                        verticesStep = 10;
+                        break;
+                }
+
+                var verticesCount = verticesArray.length / verticesStep;
+                var facesCount = indicesArray.length / 3;
+                var mesh = new SoftEngine.Mesh(jsonObject.meshes[meshIndex].name, verticesCount, facesCount);
+
+                // Vertices
+                for (var index = 0; index < verticesCount; index++) {
+                    var x = verticesArray[index * verticesStep];
+                    var y = verticesArray[index * verticesStep + 1];
+                    var z = verticesArray[index * verticesStep + 2];
+                    mesh.Vertices[index] = new BABYLON.Vector3(x, y, z);
+                }
+
+                // Faces
+                for (var index = 0; index < facesCount; index++) {
+                    var a = indicesArray[index * 3];
+                    var b = indicesArray[index * 3 + 1];
+                    var c = indicesArray[index * 3 + 2];
+                    mesh.Faces[index] = {
+                        A: a,
+                        B: b,
+                        C: c
+                    };
+                }
+
+                var position = jsonObject.meshes[meshIndex].position;
+                mesh.Position = new BABYLON.Vector3(position[0], position[1], position[2]);
+                meshes.push(mesh);
+            }
+            return meshes;
+        };
 
        	Device.prototype.clear = function() {
        	    this.workingContext.clearRect(0, 0, this.workingWidth, this.workingHeight);
        	    this.backbuffer = this.workingContext.getImageData(0, 0, this.workingWidth, this.workingHeight);
+
+            for (var i = 0; i < this.depthbuffer.length; i++) {
+                this.depthbuffer[i] = 10000000;
+            }
        	};
 
        	Device.prototype.present = function() {
        	    this.workingContext.putImageData(this.backbuffer, 0, 0);
        	};
 
-       	Device.prototype.putPixel = function(x, y, color) {
+       	Device.prototype.putPixel = function(x, y, z, color) {
        	    this.backbufferdata = this.backbuffer.data;
-       	    var index = ((x >> 0) + (y >> 0) * this.workingWidth) * 4;
+       	    var index = ((x >> 0) + (y >> 0) * this.workingWidth);
+            var index4 = index * 4
 
-       	    this.backbufferdata[index] = color.r * 255;
-       	    this.backbufferdata[index + 1] = color.g * 255;
-       	    this.backbufferdata[index + 2] = color.b * 255;
-       	    this.backbufferdata[index + 3] = color.a * 255;
+            if (this.depthbuffer[index] < z) {
+                return;
+            }
+
+            this.depthbuffer[index] = z;
+
+       	    this.backbufferdata[index4] = color.r * 255;
+       	    this.backbufferdata[index4 + 1] = color.g * 255;
+       	    this.backbufferdata[index4 + 2] = color.b * 255;
+       	    this.backbufferdata[index4 + 3] = color.a * 255;
        	};
 
 
        	Device.prototype.project = function(coord, transMat) {
        	    var point = BABYLON.Vector3.TransformCoordinates(coord, transMat);
-       	    var x = point.x * this.workingWidth + this.workingWidth / 2.0 >> 0;
-       	    var y = -point.y * this.workingHeight + this.workingHeight / 2.0 >> 0;
+       	    var x = point.x * this.workingWidth + this.workingWidth / 2.0;
+       	    var y = -point.y * this.workingHeight + this.workingHeight / 2.0;
 
-       	    return (new BABYLON.Vector2(x, y));
+       	    return (new BABYLON.Vector3(x, y, point.z));
        	}
 
-       	Device.prototype.drawPoint = function(point) {
+        Device.prototype.clamp = function(value, min, max) {
+            if (typeof min === "undefined") { min = 0; }
+            if (typeof max === "undefined") { max = 1; }
+            return Math.max(min, Math.min(value, max));
+        }
+
+        Device.prototype.interpolate = function(min, max, gradient) {
+            return min + (max - min) * this.clamp(gradient);
+        }
+
+        // papb -> pcpd
+        // Y is used to compute gradient to get starting X (sx) and ending X (ex)
+        Device.prototype.processScanLine = function(y, pa, pb, pc, pd, color) {
+            var gradient1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
+            var gradient2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+
+            var sx = this.interpolate(pa.x, pb.x, gradient1) >> 0;
+            var ex = this.interpolate(pc.x, pd.x, gradient2) >> 0;
+
+            var z1 = this.interpolate(pa.z, pb.z, gradient1);
+            var z2 = this.interpolate(pc.z, pd.z, gradient2);
+
+            for (var x = sx; x < ex; x++) {
+                var gradient = (x - sx) / (ex - sx);
+                var z = this.interpolate(z1, z2, gradient);
+                this.drawPoint(new BABYLON.Vector3(x, y, z), color);
+            }
+        };
+
+        Device.prototype.drawTriangle = function(p1, p2, p3, color) {
+            // Sort points to p1 always up and p2 between p1 and p3
+            if (p1.y > p2.y) {
+                var temp = p2;
+                p2 = p1;
+                p1 = temp;
+            }
+            if (p2.y > p3.y) {
+                var temp = p2;
+                p2 = p3;
+                p3 = temp;
+            }
+            if (p1.y > p2.y) {
+                var temp = p2;
+                p2 = p1;
+                p1 = temp;
+            }
+
+            // inverse slopes
+            var dP1P2, dP1P3;
+
+            if (p2.y - p1.y > 0) {
+                dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
+            }
+            else {
+                dP1P2 = 0;
+            }
+
+            if (p3.y - p1.y > 0) {
+                dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
+            }
+            else {
+                dP1P3 = 0;
+            }
+
+            if (dP1P2 > dP1P3) {
+                for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                    if (y < p2.y) {
+                        this.processScanLine(y, p1, p3, p1, p2, color);
+                    }
+                    else {
+                        this.processScanLine(y, p1, p3, p2, p3, color);
+                    }
+                }
+            }
+            else {
+                for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                    if (y < p2.y) {
+                        this.processScanLine(y, p1, p2, p1, p3, color);
+                    }
+                    else {
+                        this.processScanLine(y, p2, p3, p1, p3, color);
+                    }
+                }
+            }
+
+        }
+
+       	Device.prototype.drawPoint = function(point, color) {
        	    if (point.x >= 0 && point.y >= 0 && point.x < this.workingWidth
        	         && point.y < this.workingHeight) {
-                       this.putPixel(point.x, point.y, new BABYLON.Color4(1, 0, 1, 1));
+                this.putPixel(point.x, point.y, point.z, color);
        	    }
        	};
 
@@ -121,18 +287,17 @@ var SoftEngine;
               		var transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
 
               		for (var indexFaces = 0; indexFaces < currentMesh.Faces.length; indexFaces++) {
-                    var currentFace = currentMesh.Faces[indexFaces];
-                    var vertexA = currentMesh.Vertices[currentFace.A];
-                    var vertexB = currentMesh.Vertices[currentFace.B];
-                    var vertexC = currentMesh.Vertices[currentFace.C];
+                        var currentFace = currentMesh.Faces[indexFaces];
+                        var vertexA = currentMesh.Vertices[currentFace.A];
+                        var vertexB = currentMesh.Vertices[currentFace.B];
+                        var vertexC = currentMesh.Vertices[currentFace.C];
 
-                    var pixelA = this.project(vertexA, transformMatrix);
-                    var pixelB = this.project(vertexB, transformMatrix);
-                    var pixelC = this.project(vertexC, transformMatrix);
+                        var pixelA = this.project(vertexA, transformMatrix);
+                        var pixelB = this.project(vertexB, transformMatrix);
+                        var pixelC = this.project(vertexC, transformMatrix);
 
-                    this.drawBresenhamLine(pixelA, pixelB);
-                    this.drawBresenhamLine(pixelB, pixelC);
-                    this.drawBresenhamLine(pixelC, pixelA);
+                        var color = 0.25 + ((indexFaces % currentMesh.Faces.length) / currentMesh.Faces.length) * 0.75;
+                        this.drawTriangle(pixelA, pixelB, pixelC, new BABYLON.Color4(color, color, color, 1));
               		}
        	    }
        	};
